fab60864b1853c2a93aff85d1e9f883526a7f5da
Rodrigo Barreira <rodrigo.abarreira@gmail.com>
2021-11-10 10:42:30 -0300

    Implementando nomes as rotas e rota de fallback

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-/*
+
 Route::get('/', function () {
     return 'Olá, seja bem-vindo ao curso!';
 });
-
-*/
-
-Route::get('/', 'PrincipalController@principal');
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-//nome, categoria, assunto, mensagem
-
-
-/*
 
 Acrescentando '?' no fim do parâmetro a fim de ser opcional
 
@@ -40,9 +27,6 @@ function(
     ){
         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
 });
-*/
-
-/*
 
 where = especificando o tipo de caractere para que seja ao menos um dos tipos,
 sendo where('parâmetro','opção desejada')
@@ -55,4 +39,34 @@ function(
         echo "Estamos aqui: $nome - $categoria_id";
     }
 )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+
 */
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+
+});
+
+
+Route::get('/rota1', function(){
+    echo "Rota 1";
+})->name('site.rota1');
+
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+//Route::redirect('/rota2','/rota1');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para página inicial';
+});
